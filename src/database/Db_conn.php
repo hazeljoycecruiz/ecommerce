3dcc0911b79ab41c047670ea20629331f25b56b0
hazeljoycecruiz <137643047+hazeljoycecruiz@users.noreply.github.com>
2024-11-15 00:23:30 +0800

    4

--- src/database/Db_conn.php
+++ src/database/Db_conn.php
@@ -1,4 +1,29 @@
 <?php
+// class Database {
+//     private $host = "127.0.0.1"; 
+//     private $username = "root"; 
+//     private $password = "";     
+//     private $db_name = "a_ecommerce";  
+//     public $conn;
+
+//     public function connect() {
+//         $this->conn = null;
+
+//         try {
+          
+//             $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
+//             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//             echo "Connected successfully";  // Add a message to confirm connection success
+//         } catch(PDOException $e) {
+//             // Capture and display error messages
+//             echo "Connection error: " . $e->getMessage();
+//         }
+
+//         return $this->conn;
+//     }
+    
+// }
+
 class Database {
     private $host = "127.0.0.1"; 
     private $username = "root"; 
@@ -10,10 +35,8 @@ class Database {
         $this->conn = null;
 
         try {
-          
             $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";  // Add a message to confirm connection success
         } catch(PDOException $e) {
             // Capture and display error messages
             echo "Connection error: " . $e->getMessage();
@@ -21,6 +44,6 @@ class Database {
 
         return $this->conn;
     }
-    
 }
+
 ?>
